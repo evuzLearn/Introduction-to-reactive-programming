94aa5ea1588c493c0b5bbfcf55de6ab5beee4029
evuz <jesus_jgb_45@hotmail.com>
2017-08-30 14:58:55 +0200

    mergeMap

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const input = document.querySelector('input');
-const observable = Rx.Observable.of(1, 2, 3, 4, 5);
+const input1 = document.querySelector('.input1');
+const input2 = document.querySelector('.input2');
 
-observable
-    .scan((total, currentValue) => total + currentValue, 0)
-    .reduce((total, currentValue) => total + currentValue, 0)
-    .subscribe({
-        next: (value) => console.log(value),
-        error: (err) => console.log(err)
-    });
+var span = document.querySelector('span');
+
+const obs1 = Rx.Observable.fromEvent(input1, 'input');
+const obs2 = Rx.Observable.fromEvent(input2, 'input')
+
+obs1.mergeMap((ev1) => {
+    return obs2.map(ev2 => ev1.target.value + ' ' + ev2.target.value);
+}).subscribe((combined) => span.textContent = combined);
+    // .subscribe({
+    //     next: (event) => span.textContent = event.target.value,
+    //     error: (err) => console.log(err)
+    // });
+
+
+// obs
+// .subscribe({
+//     next: (event) => span.textContent = event.target.value,
+//     error: (err) => console.log(err)
+// });

dc5b70c76390b3d2ce0bcaabdf18824a6ce87cab
evuz <jesus_jgb_45@hotmail.com>
2017-08-30 14:31:45 +0200

    scan & reduce

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const input = document.querySelector('input');
-const observable = Rx.Observable.fromEvent(input, 'input');
+const observable = Rx.Observable.of(1, 2, 3, 4, 5);
 
 observable
-    .debounceTime(500)
-    .map((ev) => ev.target.value)
-    .distinctUntilChanged()
+    .scan((total, currentValue) => total + currentValue, 0)
+    .reduce((total, currentValue) => total + currentValue, 0)
     .subscribe({
         next: (value) => console.log(value),
         error: (err) => console.log(err)

9ed7e28f13fe92c26374036d84be671ac757ad6a
evuz <jesus_jgb_45@hotmail.com>
2017-08-31 10:11:25 +0200

    SwitchMap

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const input1 = document.querySelector('.input1');
-const input2 = document.querySelector('.input2');
+const button = document.querySelector('button');
 
-var span = document.querySelector('span');
+const obs1 = Rx.Observable.fromEvent(button, 'click');
+const obs2 = Rx.Observable.interval(1000);
 
-const obs1 = Rx.Observable.fromEvent(input1, 'input');
-const obs2 = Rx.Observable.fromEvent(input2, 'input')
-
-obs1.mergeMap((ev1) => {
-    return obs2.map(ev2 => ev1.target.value + ' ' + ev2.target.value);
-}).subscribe((combined) => span.textContent = combined);
-    // .subscribe({
-    //     next: (event) => span.textContent = event.target.value,
-    //     error: (err) => console.log(err)
-    // });
-
-
-// obs
-// .subscribe({
-//     next: (event) => span.textContent = event.target.value,
-//     error: (err) => console.log(err)
-// });
+obs1.switchMap((ev) => {
+    return obs2
+}).subscribe((value) => console.log(value));
